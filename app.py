ed681daf331f7b9b58550f4054f552c6ee0de2fd
twentytwentythree <sten8791@colorado.edu>
2026-04-06 21:35:28 +0000

    Update db_test route

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Stephen Enriquez in 3308'
 
-@app.route('/db test')
+@app.route('/db_test')
 def testing():
     conn = psycopg2.connect("postgresql://latencies_database_user:rDYr0nRGIeTKrQA1dnhEYyH35oiW2ME1@dpg-d79k70khg0os73e7ev00-a/latencies_database")
     conn.close()
